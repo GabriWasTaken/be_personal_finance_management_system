3d8e5eab1c4351330b6b7a7176d84c763c1bc2ca
gabriele <gabrieletorta@gmail.com>
2025-03-25 00:40:33 +0100

    feature: added prehandler and todo

--- server.js
+++ server.js
@@ -6,13 +6,28 @@ import fastifyCors from '@fastify/cors'
 
 /**
  * @type {import('fastify').FastifyInstance} Instance of Fastify
- */
+*/
+
 const fastify = Fastify({
   logger: true
 })
 
-fastify.register(fastifyCors, { origin: '*' })
+/*
+  * TODO:
+  * introduce typescript
+  * check for JWT validation
+  * handle pagination
+*/
+
+fastify.register(fastifyCors, { origin: process.env.FE_ORIGIN })
 fastify.register(dbConnector)
+
+fastify.addHook('preHandler', (request, reply, done) => {
+  console.log("prehandler")
+  // request.headers check for jwt token and valite through logto
+  done()
+})
+
 fastify.register(account)
 
 fastify.listen({ port: 3001 }, function (err, address) {
