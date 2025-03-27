13e3e5a261e82a4a458fd53b1fb9331c59c1c6d3
gabriele <gabrieletorta@gmail.com>
2025-03-27 22:01:50 +0100

    feature: jwt validation

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import Fastify from 'fastify'
 import account from './routes/account.js'
 import dbConnector from './db-connector.js'
 import fastifyCors from '@fastify/cors'
+import fjwt from '@fastify/jwt'
+import buildGetJwks from 'get-jwks'
 
 /**
  * @type {import('fastify').FastifyInstance} Instance of Fastify
@@ -15,16 +17,31 @@ const fastify = Fastify({
 /*
   * TODO:
   * introduce typescript
-  * check for JWT validation
-  * handle pagination
 */
+
+const getJwks = buildGetJwks({ jwksPath: "/jwks" });
+
+fastify.register(fjwt, {
+  decode: { complete: true },
+  secret: (request, token) => {
+    return getJwks.getPublicKey({
+      domain: process.env.JWT_ISSUER,
+      alg: 'ES384',
+      kid: 'LCPrW7v2KI9_mdQCLAdymkJvjDBV07Z_KUndcLPPlUE',
+    })
+  }
+})
 
 fastify.register(fastifyCors, { origin: process.env.FE_ORIGIN })
 fastify.register(dbConnector)
 
 fastify.addHook('preHandler', (request, reply, done) => {
-  console.log("prehandler")
-  // request.headers check for jwt token and valite through logto
+  request.jwtVerify().then(() => {
+    console.log("token verified")
+  }).catch((err) => {
+    reply.send(err)
+  })
+
   done()
 })
 
