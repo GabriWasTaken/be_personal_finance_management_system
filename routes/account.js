25a73ae597cd7457ba9e5c66272562574da1cd64
gabriele <gabrieletorta@gmail.com>
2025-03-29 18:50:59 +0100

    bugfix: pagination

--- routes/account.js
+++ routes/account.js
@@ -25,13 +25,21 @@ async function routes (fastify, options) {
     return result
   })
 
-  fastify.get('/accounts', (req, reply) => {
-    fastify.pg.query(
-      'SELECT * FROM accounts',
-      function onResult (err, result) {
-        reply.send(err || {rows:result.rows, rowCount: result.rowCount});
-      }
+  fastify.get('/accounts', async (req, reply) => {
+    let rowsNumber = 0;
+    let result;
+
+    const client = await fastify.pg.connect()
+    const { rows } = await client.query('SELECT COUNT(*) AS total_rows FROM accounts');
+    
+    rowsNumber = rows[0].total_rows;
+
+    result = await client.query(
+      'SELECT * FROM accounts OFFSET $1 LIMIT $2', [Number(req.query.page) * Number(req.query.limit), Number(req.query.limit)],
     )
+    
+    client.release()
+    return {rows:result.rows, rowCount: rowsNumber}
   })
 
   fastify.get('/dashboard', (req, reply) => {
@@ -58,11 +66,11 @@ async function routes (fastify, options) {
 
     if(!req.query.id_account){
       result =await client.query(
-        'SELECT financials.*, accounts.name as account_name FROM financials JOIN accounts ON financials.id_account = accounts.id OFFSET $1 LIMIT $2', [Number(req.query.page), Number(req.query.limit)],
+        'SELECT financials.*, accounts.name as account_name FROM financials JOIN accounts ON financials.id_account = accounts.id OFFSET $1 LIMIT $2', [Number(req.query.page) * Number(req.query.limit), Number(req.query.limit)],
       )
     } else {
       result = await client.query(
-        'SELECT financials.*, accounts.name as account_name FROM financials JOIN accounts ON financials.id_account = accounts.id WHERE id_account=$1 OFFSET $2 LIMIT $3', [req.query.id_account, Number(req.query.page), Number(req.query.limit)],
+        'SELECT financials.*, accounts.name as account_name FROM financials JOIN accounts ON financials.id_account = accounts.id WHERE id_account=$1 OFFSET $2 LIMIT $3', [req.query.id_account, Number(req.query.page) * Number(req.query.limit), Number(req.query.limit)],
       )
     }
 
