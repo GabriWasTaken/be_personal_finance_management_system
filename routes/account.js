fdf032a7e966143b9ae1c79da0cceb3e4c28715d
gabriele <gabrieletorta@gmail.com>
2025-04-09 22:35:03 +0200

    feat: dashboard aggregations

--- routes/account.js
+++ routes/account.js
@@ -20,7 +20,7 @@ async function routes (fastify, options) {
 
     const client = await fastify.pg.connect()
     const { rows } = await client.query('SELECT COUNT(*) AS total_rows FROM accounts WHERE user_id=$1', [req.user.id]);
-    
+
     rowsNumber = rows[0].total_rows;
 
     result = await client.query(
@@ -30,24 +30,86 @@ async function routes (fastify, options) {
                WHEN financials.type = 'expense' THEN -financials.amount
                ELSE 0
            END) AS net_total
-      FROM financials 
-      JOIN accounts ON accounts.id = financials.id_account
+      FROM financials, accounts
       WHERE accounts.user_id=$1
       GROUP BY accounts.id
       OFFSET $2 LIMIT $3`, [req.user.id, Number(req.query.page) * Number(req.query.limit), Number(req.query.limit)],
     )
-    
+
     client.release()
     return {rows:result.rows, rowCount: rowsNumber}
   })
 
-  fastify.get('/dashboard', (req, reply) => {
-    fastify.pg.query(
-      'SELECT * FROM accounts',
-      function onResult (err, result) {
-        reply.send({result:"Dashboard", rowCount: 1});
-      }
+  fastify.get('/dashboard', async (req, reply) => {
+    const client = await fastify.pg.connect()
+    const allTimeResults = await client.query(
+      `SELECT
+       SUM(CASE 
+               WHEN financials.type = 'income' THEN financials.amount
+               ELSE 0
+           END) AS total_income,
+       SUM(CASE 
+               WHEN financials.type = 'expense' THEN financials.amount
+               ELSE 0
+           END) AS total_expense
+      FROM financials 
+      WHERE financials.user_id=$1`,
+      [req.user.id],
     )
+
+    const today = new Date();
+    const year = today.getFullYear();
+
+    const prevYearResults = await client.query(
+      `SELECT
+      SUM(CASE 
+            WHEN financials.type = 'income' THEN financials.amount
+            ELSE 0
+            END) AS total_income,
+      SUM(CASE 
+            WHEN financials.type = 'expense' THEN financials.amount
+            ELSE 0
+            END) AS total_expense
+      FROM financials 
+      WHERE financials.user_id = $1
+      AND financials.transaction_date >= '${year - 1}-01-01'
+      AND financials.transaction_date < '${year}-01-01';`,
+      [req.user.id],
+    );
+
+    const YTDResults = await client.query(
+      `SELECT
+      SUM(CASE 
+            WHEN financials.type = 'income' THEN financials.amount
+            ELSE 0
+            END) AS total_income,
+      SUM(CASE 
+            WHEN financials.type = 'expense' THEN financials.amount
+            ELSE 0
+            END) AS total_expense
+      FROM financials 
+      WHERE financials.user_id = $1
+      AND financials.transaction_date >= '${year}-01-01'
+      AND financials.transaction_date < CURRENT_DATE;`,
+      [req.user.id],
+    );
+
+    client.release();
+
+    return { 
+      allTime: {
+        total_expense: allTimeResults.rows[0].total_expense, 
+        total_income: allTimeResults.rows[0].total_income
+      }, 
+      prevYear: { 
+        total_expense: prevYearResults.rows[0].total_expense, 
+        total_income: prevYearResults.rows[0].total_income 
+      },
+      YTD: {
+        total_expense: YTDResults.rows[0].total_expense, 
+        total_income: YTDResults.rows[0].total_income 
+      }
+    }
   })
 
   fastify.get('/financials', async (req, reply) => {
@@ -59,11 +121,11 @@ async function routes (fastify, options) {
       'SELECT COUNT(*) AS total_rows FROM financials WHERE user_id=$1' + (req.query.id_account ? ' AND id_account=$2' : ''),
       req.query.id_account ? [req.user.id, req.query.id_account] : [req.user.id]
     )
-    
+
     rowsNumber = rows[0].total_rows;
 
-    if(!req.query.id_account){
-      result =await client.query(
+    if (!req.query.id_account) {
+      result = await client.query(
         `SELECT financials.*, accounts.name as account_name, categories.name as category_name, subcategories.name as subcategory_name
         FROM financials 
         JOIN accounts ON financials.id_account = accounts.id 
@@ -71,7 +133,7 @@ async function routes (fastify, options) {
         JOIN subcategories ON financials.subcategory_id = subcategories.id 
         WHERE financials.user_id=$1 
         ORDER BY transaction_date DESC 
-        OFFSET $2 LIMIT $3`, 
+        OFFSET $2 LIMIT $3`,
         [req.user.id, Number(req.query.page) * Number(req.query.limit), Number(req.query.limit)],
       )
     } else {
@@ -89,14 +151,14 @@ async function routes (fastify, options) {
     }
 
     client.release()
-    return {rows:result.rows, rowCount: rowsNumber}
+    return { rows: result.rows, rowCount: rowsNumber }
   })
 
 
   fastify.post('/financials', async (req, reply) => {
-    const { amount, name, id_account, id_category, id_subcategory, transactionDate, type, id_account_to} = req.body;
+    const { amount, name, id_account, id_category, id_subcategory, transactionDate, type, id_account_to } = req.body;
     let result;
-    if (type !== "transfer"){
+    if (type !== "transfer") {
       result = await fastify.pg.query(
         'INSERT INTO financials (name, amount, id_account, user_id, category_id, subcategory_id, transaction_date, type, is_transfer) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
         [name, amount, id_account, req.user.id, id_category, id_subcategory, transactionDate, type, false],
@@ -130,15 +192,15 @@ async function routes (fastify, options) {
 
     const client = await fastify.pg.connect()
     const { rows } = await client.query('SELECT COUNT(*) AS total_rows FROM categories WHERE user_id=$1', [req.user.id]);
-    
+
     rowsNumber = rows[0].total_rows;
 
     result = await client.query(
       'SELECT * FROM categories WHERE user_id=$1 OFFSET $2 LIMIT $3', [req.user.id, Number(req.query.page) * Number(req.query.limit), Number(req.query.limit)],
     )
-    
+
     client.release()
-    return {rows:result.rows, rowCount: rowsNumber}
+    return { rows: result.rows, rowCount: rowsNumber }
   })
 
   fastify.post('/categories', async (req, reply) => {
@@ -156,15 +218,15 @@ async function routes (fastify, options) {
 
     const client = await fastify.pg.connect()
     const { rows } = await client.query('SELECT COUNT(*) AS total_rows FROM subcategories WHERE user_id=$1 AND category_id=$2', [req.user.id, req.query.id_category]);
-    
+
     rowsNumber = rows[0].total_rows;
 
     result = await client.query(
       'SELECT * FROM subcategories WHERE user_id=$1 AND category_id=$2 OFFSET $3 LIMIT $4', [req.user.id, req.query.id_category, Number(req.query.page) * Number(req.query.limit), Number(req.query.limit)],
     )
-    
+
     client.release()
-    return {rows:result.rows, rowCount: rowsNumber}
+    return { rows: result.rows, rowCount: rowsNumber }
   })
 
   fastify.post('/subcategories', async (req, reply) => {
